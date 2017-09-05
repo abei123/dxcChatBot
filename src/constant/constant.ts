8cf06473aed1b5892b7d3922e2f40cd53c5b6f4d
abram-l-garcia <abram-l.garcia@hpe.com>
2017-09-05 10:51:12 -0500

    test

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,5 +1,5 @@
 export const URL = {
-  //patientUrl: 'http://52.163.246.246:8810/Patient',
-  patientUrl: '/Patient',
+  patientUrl: 'http://52.163.246.246:8810/Patient',
+  //patientUrl: '/Patient',
   conversationUrl: 'https://directline.botframework.com/v3/directline/conversations'
 }
